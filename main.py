95de9459c744fb997516c2f6a6d236c9ee9cb50c
RCRalph <r.chrzanowski.priv@gmail.com>
2021-01-27 17:09:26 +0100

    Added non-repeat selecting
    https://www.youtube.com/watch?v=DLzxrzFCyOs

--- main.py
+++ main.py
@@ -2,20 +2,41 @@ import discord
 import random
 import json
 
+from decouple import config
+
 class Client(discord.Client):
+    data = {}
+    chosenIDs = []
+
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        print('Logged on as {}!'.format(self.user))
+
+        dataFromFile = json.load(open('data.json'))
+        self.data = {
+            key: dataFromFile[key]
+            for key in dataFromFile
+            if dataFromFile[key] != None
+        }
+
     async def on_message(self, message):
         if message.author == self.user:
             return
+        
         if message.content.startswith('$losuj') or message.content.startswith('$pseudolosuj'):
-            with open('data.json') as file:
-                data = json.load(file)
-                num = random.randint(1, int(data['liczba osob']))
-                while data[str(num)] == 'wykreslony':
-                    num = random.randint(1, int(data['liczba osob']))
-                print(f'{num} - {data[str(num)]}')
-                await message.channel.send(f'{num} - {data[str(num)]}')
+            notChosen = {
+                key: self.data[key]
+                for key in self.data
+                if key not in self.chosenIDs
+            }
+
+            chosen = random.choice(list(notChosen.items()))
+            if (len(self.chosenIDs) < len(self.data)):
+                self.chosenIDs.append(chosen[0])
+            else:
+                self.chosenIDs = []
+
+            print('{} - {}'.format(chosen[0], chosen[1]))
+            await message.channel.send('{} - {}'.format(chosen[0], chosen[1]))
 
 client = Client()
-client.run('token')
+client.run(config('BOT_TOKEN'))
